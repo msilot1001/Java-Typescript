15c2660a92f8907d59325cef1541a94d9836abb7
msilot1001 <msilot1001@gmail.com>
2022-06-23 17:26:58 +0900

    Init2

--- psqltest1/index.js
+++ psqltest1/index.js
@@ -1,40 +1,46 @@
-const { Client } = require('pg');
-const PGCli = new Client()
+const { Client } = require("pg");
+const PGCli = new Client();
 require("dotenv").config();
 const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout,
 });
 
-console.log('PsqlTester @v0.1.0 By Msilot1001')
+console.log("PsqlTester @v0.1.0 By Msilot1001");
 DB_Connect();
 //Interface_Home();
 
 async function DB_Connect() {
-    const pg = new Client({
-        user: 'rbbfyabymoibyh',
-        host: 'ec2-3-228-222-169.compute-1.amazonaws.com',
-        database: 'ddndpaaev1f99f',
-        password: 'd99ca263e02e4c8f700978deb0152a4a46127f6d667bc4d7dc6c8ee31ea1cac8',
-        port: 5432,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
+	const pg = new Client({
+		user: "hololo",
+		host: "hololo",
+		database: "hololo",
+		password: "hololo",
+		port: 5432,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
 
-    try {
-        await pg.connect(err => {
-            if (err) { console.log(err); }
-            else { console.log('Psql connect success'); queryDatabase(); /*Interface_Home();*/ }
-        });
-    }
-    catch (e) { console.error(e); }
+	try {
+		await pg.connect((err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("Psql connect success");
+				queryDatabase(); /*Interface_Home();*/
+			}
+		});
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 //#region Interface
 async function Interface_Home() {
-    rl.question(`
+	rl.question(
+		`
 Select Function to Execute 
 
 (1) Add User
@@ -46,20 +52,20 @@ Select Function to Execute
 (4) Reset Table
 
 (5) End Session
-`, function (name) {
-        console.log(`input : ${name}`);
+`,
+		function (name) {
+			console.log(`input : ${name}`);
 
-        switch (name) {
-            case "1":
+			switch (name) {
+				case "1":
+					break;
+			}
 
-                break;
-        }
-
-        Interface_Home();
-    });
+			Interface_Home();
+		}
+	);
 }
 //#endregion
-
 
 /*
     Query 부분 사용법
@@ -71,21 +77,21 @@ Select Function to Execute
 //#region Query
 
 async function Query_Execute(query) {
-    await PGCli.query(query, (err, res) => {
-        console.log(`Query_Execute`);
-        if (err) {
-            console.log(err.stack)
-        } else {
-            console.log(res.rows[0])
-        }
-        client.end();
-    });
+	await PGCli.query(query, (err, res) => {
+		console.log(`Query_Execute`);
+		if (err) {
+			console.log(err.stack);
+		} else {
+			console.log(res.rows[0]);
+		}
+		client.end();
+	});
 }
 
 async function Query_ResetTable() {
-    // username 길면 컷하기 명심!
-    // username 오류나면 글자 제한 생각해보기 
-    const query = `
+	// username 길면 컷하기 명심!
+	// username 오류나면 글자 제한 생각해보기
+	const query = `
         DROP TABLE IF EXISTS userinfo; 
         CREATE TABLE userinfo (
             "user_id" serial PRIMARY KEY, 
@@ -97,8 +103,8 @@ async function Query_ResetTable() {
 //#endregion
 
 async function queryDatabase() {
-    console.log(`:102`)
-    const query = `CREATE TABLE IF NOT EXISTS usertbl1 ("user_id" bigint PRIMARY KEY, 
+	console.log(`:102`);
+	const query = `CREATE TABLE IF NOT EXISTS usertbl1 ("user_id" bigint PRIMARY KEY, 
 "user_name" VARCHAR (50) UNIQUE NOT NULL,
 "wallet" INT NOT NULL,
 "bank" INT NOT NULL );
@@ -108,14 +114,13 @@ DELETE FROM usertbl1 WHERE user_id=780771337332981780;
 
 INSERT INTO usertbl1(user_id, user_name, wallet, bank) VALUES(780771337332981780, 'Msilot1001', 20220307 , 2141 ) RETURNING *;`;
 
-    await PGCli.query(query, (err, res) => {
-        console.log(`:115`);
-        if (err) {
-            console.log(err.stack)
-        } else {
-            console.log(res.rows[0])
-        }
-        client.end();
-    });
+	await PGCli.query(query, (err, res) => {
+		console.log(`:115`);
+		if (err) {
+			console.log(err.stack);
+		} else {
+			console.log(res.rows[0]);
+		}
+		client.end();
+	});
 }
-
